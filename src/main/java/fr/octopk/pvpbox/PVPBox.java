83ef22ec7a4045920b138f90d1c915aa85b05925
OctoPK <armand@biguey.com>
2025-11-13 10:36:36 +0100

    initial commit

---
+++ src/main/java/fr/octopk/pvpbox/PVPBox.java
@@ -0,0 +1,16 @@
+package fr.octopk.pvpbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PVPBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
